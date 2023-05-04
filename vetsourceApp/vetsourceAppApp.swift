97e9f899621c46b891619e00dd088ea1a99c7a4d
xkevin190 <kevinvelasco193@gmail.com>
2023-05-04 17:16:36 -0400

    Initial Commit

---
+++ vetsourceApp/vetsourceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  vetsourceAppApp.swift
+//  vetsourceApp
+//
+//  Created by Kevin Velasco on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct vetsourceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
